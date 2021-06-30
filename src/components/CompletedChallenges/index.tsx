fe272d37cb0b61fc9ce7621194cc703f1f2eb1d8
Garcez17 <ggarcez613@gmail.com>
2021-06-30 14:50:55 -0300

    fix completedChallenges

--- src/components/CompletedChallenges/index.tsx
+++ src/components/CompletedChallenges/index.tsx
@@ -30,30 +30,34 @@ export function CompletedChallenges() {
           <span>{cycle % 2 === 0 ? 'Descanso' : 'Foco'}</span>
         </header>
         <div>
-          <Workbar>
-            <p>{cycle > 1 ? <FiCheck size={32} /> : `${pomodoro.pom_time} min`}</p>
-          </Workbar>
-          <Breakbar>
-            <p>{cycle > 2 ? <FiCheck size={24} /> : `${pomodoro.pom_break} min` }</p>
-          </Breakbar>
-          <Workbar>
-            <p>{cycle > 3 ? <FiCheck size={32} /> : `${pomodoro.pom_time} min`}</p>
-          </Workbar>
-          <Breakbar>
-            <p>{cycle > 4 ? <FiCheck size={24} /> : `${pomodoro.pom_break} min` }</p>
-          </Breakbar>
-          <Workbar>
-            <p>{cycle > 5 ? <FiCheck size={32} /> : `${pomodoro.pom_time} min`}</p>
-          </Workbar>
-          <Breakbar>
-            <p>{cycle > 6 ? <FiCheck size={24} /> : `${pomodoro.pom_break} min` }</p>
-          </Breakbar>
-          <Workbar>
-            <p>{cycle > 7 ? <FiCheck size={32} /> : `${pomodoro.pom_time} min`}</p>
-          </Workbar>
-          <Breakbar>
-            <p>{cycle > 8 ? <FiCheck size={24} /> : `${pomodoro.pom_time + 5} min` }</p>
-          </Breakbar>
+          {pomodoro && (
+            <>
+              <Workbar>
+                <p>{cycle > 1 ? <FiCheck size={32} /> : `${pomodoro.pom_time} min`}</p>
+              </Workbar>
+              <Breakbar>
+                <p>{cycle > 2 ? <FiCheck size={24} /> : `${pomodoro.pom_break} min` }</p>
+              </Breakbar>
+              <Workbar>
+                <p>{cycle > 3 ? <FiCheck size={32} /> : `${pomodoro.pom_time} min`}</p>
+              </Workbar>
+              <Breakbar>
+                <p>{cycle > 4 ? <FiCheck size={24} /> : `${pomodoro.pom_break} min` }</p>
+              </Breakbar>
+              <Workbar>
+                <p>{cycle > 5 ? <FiCheck size={32} /> : `${pomodoro.pom_time} min`}</p>
+              </Workbar>
+              <Breakbar>
+                <p>{cycle > 6 ? <FiCheck size={24} /> : `${pomodoro.pom_break} min` }</p>
+              </Breakbar>
+              <Workbar>
+                <p>{cycle > 7 ? <FiCheck size={32} /> : `${pomodoro.pom_time} min`}</p>
+              </Workbar>
+              <Breakbar>
+                <p>{cycle > 8 ? <FiCheck size={24} /> : `${pomodoro.pom_time + 5} min` }</p>
+              </Breakbar>
+            </>
+          )}
         </div>
       </Pomodoro>
     </Container>
