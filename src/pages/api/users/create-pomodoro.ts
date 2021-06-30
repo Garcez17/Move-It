ce0089986a049bb83c7c2957cfd00154ba79be7d
Garcez17 <ggarcez613@gmail.com>
2021-06-30 13:27:43 -0300

    add vercel package

--- src/pages/api/users/create-pomodoro.ts
+++ src/pages/api/users/create-pomodoro.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/client";
 import { fauna } from "../../../services/fauna";
 import { query as q } from 'faunadb';
 
